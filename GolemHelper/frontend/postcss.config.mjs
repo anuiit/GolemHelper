de63266997d2484392fd05202ede2783655a8473
anuiit <anmounsi01@gmail.com>
2024-10-26 14:50:30 +0200

    Remove 'frontend' folder from js branch

--- GolemHelper/frontend/postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-
-
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
-
-export default config;
--- GolemHelper/frontend/src/app/page.jsx
+++
@@ -1,6 +0,0 @@
-// src/pages/index.tsx
-import PlayerDashboard from '@/pages/playerDashboard';
-
-export default function Home() {
-  return <PlayerDashboard />
-}
